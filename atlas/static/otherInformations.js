f930c96b743f3f13e063b539dbd40a747580f73d
JeromeMaruéjouls <jerome.maruejouls.pro@gmail.com>
2025-07-29 11:35:01 +0200

    meme chose pour les communes

--- atlas/static/otherInformations.js
+++ atlas/static/otherInformations.js
@@ -1,14 +1,18 @@
 /* Permet de compacter les listes d'observateurs et de communes */
 
 document.addEventListener('DOMContentLoaded', function () {
-    const btn = document.getElementById('toggle-observers-btn');
-    const more = document.getElementById('more-observers');
-    let expanded = false;
-    if (btn && more) {
-        btn.addEventListener('click', function () {
-            expanded = !expanded;
-            more.classList.toggle('d-none');
-            btn.textContent = expanded ? 'Afficher moins' : 'Afficher plus';
-        });
+    function setupToggle(btnId, targetId, showText = 'Afficher plus', hideText = 'Afficher moins') {
+        const btn = document.getElementById(btnId);
+        const target = document.getElementById(targetId);
+        let expanded = false;
+        if (btn && target) {
+            btn.addEventListener('click', function () {
+                expanded = !expanded;
+                target.classList.toggle('d-none');
+                btn.textContent = expanded ? hideText : showText;
+            });
+        }
     }
+    setupToggle('toggle-observers-btn', 'more-observers');
+    setupToggle('toggle-communes-btn', 'more-communes');
 });
