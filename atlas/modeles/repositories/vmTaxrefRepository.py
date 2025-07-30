29e2e76edb11d63ed268ecf8d70c6088b7d35aa8
geoek <jerome.maruejouls@gmail.com>
2025-07-30 11:01:18 +0200

    preparation graphique special pour insectes

--- atlas/modeles/repositories/vmTaxrefRepository.py
+++ atlas/modeles/repositories/vmTaxrefRepository.py
@@ -200,3 +200,13 @@ def get_cd_ref(connection, cd_nom):
     results = connection.execute(text(sql), cdNom=cd_nom)
     row = results.fetchone()
     return row.cd_ref
+
+def get_group1_2_3_inpn(connection, cd_nom):
+    sql = """
+        SELECT group1_inpn,group2_inpn,group3_inpn
+        FROM atlas.vm_taxref AS t
+        WHERE t.cd_nom = :cdNom
+    """
+    results = connection.execute(text(sql), cdNom=cd_nom)
+    row = results.fetchone()
+    return row.cd_ref
